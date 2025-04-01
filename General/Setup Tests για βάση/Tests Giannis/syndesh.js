61f65f48784b429fdaabe628c996c415afedd28f
JohnnyRoyal <jkazixis@gmail.com>
2025-04-01 16:09:24 +0300

    blakies

--- General/Setup Tests για βάση/Tests Giannis/syndesh.js
+++ General/Setup Tests για βάση/Tests Giannis/syndesh.js
@@ -4,7 +4,7 @@ const { MongoClient } = require("mongodb");
 const uri = "mongodb://localhost:27017";
 
 // Δημιουργία client
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri);
 
 async function run() {
     try {
